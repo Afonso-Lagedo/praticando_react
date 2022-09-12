fd3dcb9e52a6b6cf4632c0145275452d793d6e07
unknown <afonso.ur@gmail.com>
2022-09-12 18:04:16 -0300

    useState

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,14 +1,21 @@
-import TestName from "./components/name";
+import {useState} from 'react';
+
+import TestName from "./components/Name";
 
 function App (){
+  const [person, setPerson] = useState('Test'); //Test is initial State
+
+  function changeName(){
+    setPerson('Afonso');
+  }
+
   return(
     <div>
-      <h1>Component App</h1>
-      <Name/>
-      <br/>
-      <TestName person="Rafaela"/>
-      <br/>
-      <TestName person="Felipe"/>
+      <h1>Component App</h1><br/>
+      <h2>Hello: {person}</h2>
+      <button onClick={changeName}>
+        update name
+      </button>
     </div>
   );
 }
--- first-app/src/components/name.js
+++ first-app/src/components/name.js
@@ -1,20 +1,4 @@
-/*Without Props
-function TestName(){
-    return(
-      <span>Name: Rafaela</span>
-    );
-}
-*/
 
-/*With Props
-function TestName(props){
-  return(
-    <span>Name: {props.person}</span>
-  );
-}
-*/
-
-//same up
 function TestName({person}){
   return(
     <span>Name: {person}</span>
